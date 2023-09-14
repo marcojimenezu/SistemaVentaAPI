0d5fb02fe8e5033aa2bdbd3aab2e9697c1b1ddd2
Raúl Silva <raul.sr.87@gmail.com>
2023-09-14 16:17:50 -0600

    Add global error handler

--- SistemaVenta.API/Program.cs
+++ SistemaVenta.API/Program.cs
@@ -25,6 +25,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseExceptionHandler("/error");
     app.UseSwagger();
     app.UseSwaggerUI();
 }
